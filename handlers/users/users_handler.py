17eec2bae37bb172dea07be8f5f1caecd9d393e1
Vnukov Ivan <conandet@mail.ru>
2023-04-21 17:41:07 +0300

    feat: user handler

---
+++ handlers/users/users_handler.py
@@ -0,0 +1,24 @@
+from aiogram import Dispatcher
+from aiogram.types import Message
+
+from handlers.main.main_functions import main_menu
+from texts.buttons import BUTTONS
+from texts.commands import COMMANDS
+from texts.messages import MESSAGES
+
+from time import localtime
+from telethon import TelegramClient
+
+async def add_user_button(message: Message):
+    await message.answer(text=MESSAGES["user_phone"])
+
+async def phone_state(message: Message):
+    phone = message.text
+    if phone:
+        client = TelegramClient(f'base/{phone}', api_id, api_hash)
+        client.start(phone=phone)
+    await message.answer(text="Введите номер телеофна 📞")
+
+def register_users_handlers(dp: Dispatcher):
+    dp.register_message_handler(add_user_button, text=[BUTTONS["users"]])
+    dp.register_message_handler(phone_state, state=AddUserStates.phone)
