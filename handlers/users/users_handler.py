c2909c61979d49ff951f53c937cf8839a695dc1a
Vnukov Ivan <conandet@mail.ru>
2023-04-21 17:49:26 +0300

    fix: caps consts

--- handlers/users/users_handler.py
+++ handlers/users/users_handler.py
@@ -2,10 +2,11 @@ from aiogram import Dispatcher
 from aiogram.types import Message
 
 from handlers.main.main_functions import main_menu
+from states import AddUserStates
 from texts.buttons import BUTTONS
 from texts.commands import COMMANDS
 from texts.messages import MESSAGES
-
+from config import *
 from time import localtime
 from telethon import TelegramClient
 
@@ -16,7 +17,7 @@ async def add_user_button(message: Message):
 async def phone_state(message: Message):
     phone = message.text
     if phone:
-        client = TelegramClient(f'base/{phone}', api_id, api_hash)
+        client = TelegramClient(f'base/{phone}', API_ID, API_HASH)
         client.start(phone=phone)
     await message.answer(text="Введите номер телеофна 📞")
 
