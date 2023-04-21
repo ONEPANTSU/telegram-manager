179fe922e30043606bc9bde9fa7e1771d0627c38
Vnukov Ivan <conandet@mail.ru>
2023-04-22 00:18:13 +0300

    feat: add_users done?

--- handlers/users/users_handler.py
+++ handlers/users/users_handler.py
@@ -1,14 +1,22 @@
+import asyncio
+import time
+from random import shuffle
+
 from aiogram import Dispatcher
 from aiogram.dispatcher import FSMContext
 from aiogram.types import Message
 from telethon import TelegramClient
 
-from config import *
+from os import walk, remove
+from handlers.main.main_functions import main_menu
 from states import AddUserStates
 from texts.buttons import BUTTONS
 from texts.commands import COMMANDS
 from texts.messages import MESSAGES
-from useful.instruments import clients, code
+from config import *
+from telethon import TelegramClient
+
+from useful.instruments import code, clients
 
 
 async def sms_state(message: Message, state: FSMContext) -> str:
@@ -16,17 +24,14 @@ async def sms_state(message: Message, state: FSMContext) -> str:
     password = (await state.get_data())["password"]
     code[phone] = message.text
     print(code)
-    await clients[phone]._start(
-        phone=phone, password=password, code_callback=code[phone]
-    )
+    await clients[phone]._start(phone=phone, password=password, code_callback=code[phone])
     await state.finish()
 
-
 async def connect(phone, password, state):
-    client = TelegramClient(f"base/{phone}", API_ID, API_HASH)
+    client = TelegramClient(f'base/{phone}', API_ID, API_HASH)
     await client.connect()
     await client.start(phone=phone, password=password, state=state)
-
+    #await client._start(phone=phone, password=password, code_callback=sms)
 
 async def add_user_button(message: Message):
     await message.answer(text=MESSAGES["user_phone"])
@@ -47,7 +52,6 @@ async def password_state(message: Message, state: FSMContext):
     await message.answer(text=MESSAGES["user_sms"])
     await connect(phone, password, state)
 
-
 #
 # async def sms_state(message: Message, state: FSMContext):
 #     sms = message.text
@@ -58,8 +62,8 @@ async def password_state(message: Message, state: FSMContext):
 #     await message.answer(text=MESSAGES["user_password"])
 #     await state.finish()
 
-
 def register_users_handlers(dp: Dispatcher):
+
     dp.register_message_handler(add_user_button, text=[BUTTONS["users"]])
     dp.register_message_handler(phone_state, state=AddUserStates.phone)
     dp.register_message_handler(password_state, state=AddUserStates.password)
